38d462fa2fbd12708f5dc1514bb8a18b5440d366
Andrew McClain Hanold <hanoldaa@gmail.com>
2019-05-19 02:25:20 -0400

    Condensing header and navbar into one line

--- comps/header.js
+++ comps/header.js
@@ -1,4 +1,4 @@
-import Link from 'next/link'
+import LinkActive from 'next-link-active'
 
 const linkStyle = {
     marginRight: 15
@@ -7,7 +7,27 @@ const Header = () => {
     return(
         <header className="header-container">
             <div className="logo">
-                <img src="../static/logo.png"></img>
+                <img src="../static/logo2.png"></img>
+            </div>
+            <div className="header-button">
+                <LinkActive href="/" passHref>
+                    {active => <a className={active ? 'active' : ''}>Home</a>}
+                </LinkActive>
+            </div>
+            <div className="header-button">
+                <LinkActive href="/about" passHref>
+                    {active => <a className={active ? 'active' : ''}>About Us</a>}
+                </LinkActive>
+            </div>
+            <div className="header-button">
+                <LinkActive href="/services" passHref>
+                    {active => <a className={active ? 'active' : ''}>Services</a>}
+                </LinkActive>
+            </div>
+            <div className="header-button">
+                <LinkActive href="/resources" passHref>
+                    {active => <a className={active ? 'active' : ''}>Health Resources</a>}
+                </LinkActive>
             </div>
             <div className="header-button">
                 <a className="login">Log In</a>
@@ -19,11 +39,12 @@ const Header = () => {
                 .header-container {
                     display: flex;
                     flex-direction: row;
+                    font-family: Ubuntu;
                     margin-bottom: 1em;
                     padding: 0 0 0 1em;
                 }
                 .header-container .header-button {
-                    align-self: flex-start;
+                    align-self: center;
                     flex: 0 1;
                     margin: .5em 0.8em 0 0;
                 }
@@ -37,39 +58,47 @@ const Header = () => {
                     width: 100%;
                     height: 100%;
                 }
-                a {
+                .header-button a {
+                    color: #444;
                     cursor: pointer;
                     display: inline-block;
                     font-family: Ubuntu;
-                    font-size: 1.2em;
                     margin: 0 .25em;
                     text-decoration: none;
                     white-space: nowrap;
                 }
-                a.register {
+                .header-button a.active {
+                    border-bottom: 2px solid #05A5D1;
+                    color: #05A5D1;
+                }
+                .header-button a.register {
                     background-color: #05A5D1;
                     border-radius: 2em;
                     color: white;
-                    padding: .2em .75em;
+                    margin-left: 0;
+                    padding: .4em .75em;
                     -webkit-transition: filter .3s;
                     -moz-transition: filter .3s;
                     -o-transition: filter .3s;
                     transition: filter .3s;
                 }
-                a.login {
+                .header-button a.login {
                     background-color: tranparent;
-                    border-bottom: 2px solid #333;
+                    border: 1px solid black;
+                    border-radius: 2em;
                     color: #333;
-                    padding: 0;
+                    margin-left: 1em;
+                    margin-right: 0;
+                    padding: .4em .75em;
                     -webkit-transition: color .3s, border-bottom-color .3s;
                     -moz-transition: color .3s, border-bottom-color .3s;
                     -o-transition: color .3s, border-bottom-color .3s;
                     transition: color .3s, border-bottom-color .3s;
                 }
-                a.register:hover {
+                .header-button a.register:hover {
                     filter: brightness(80%);
                 }
-                a.login:hover {
+                .header-button a.login:hover {
                     color: #000;
                     border-bottom-color: #000;
                 }
@@ -81,7 +110,7 @@ const Header = () => {
                     }
                     .header-container .header-button {
                         flex: 0 1;
-                        align-self: flex-start;
+                        align-self: center;
                         margin: .5em 0.8em 0 0;
                     }
                 }
--- comps/mylayout.js
+++ comps/mylayout.js
@@ -1,5 +1,4 @@
 import Header from './header'
-import Navbar from './navbar'
 
 const Layout =  props => {
     return (
@@ -7,7 +6,6 @@ const Layout =  props => {
             <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Ubuntu" />
             <link href="https://fonts.googleapis.com/css?family=Open+Sans&display=swap" rel="stylesheet" />
             <Header />
-            <Navbar />
             <div className="page">
                 {props.children}
             </div>
